1bcf4c1e1d30250593d8d3d2524d4088d8fb269c
Enno Hyttrek <57143243+ennohyttrek@users.noreply.github.com>
2023-03-29 11:42:14 +0200

    7.0

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 
 function myCustomTheme_stylesheets() {
     wp_enqueue_style( 'style', get_stylesheet_uri());
+    wp_enqueue_style( 'lecker', get_template_directory_uri() . '/assets/fonts/delicious-handrawn/font-style.css' );
+    wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/fonts/fontawesome/css/all.css' );
 }
 add_action( 'wp_enqueue_scripts', 'myCustomTheme_stylesheets' );
 
