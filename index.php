480c03f720d6dca99d0e5240f20f0fe860b77090
Enno Hyttrek <57143243+ennohyttrek@users.noreply.github.com>
2023-03-30 13:04:18 +0200

    8.2

--- index.php
+++ index.php
@@ -31,10 +31,10 @@
             if ( have_posts() ) :
                 while ( have_posts() ) : the_post();
         ?>
-        <article>
-            <h2 class="my-post-title"><?php the_title(); ?></h2>
+
+<!--            <h2 class="my-post-title"><?php the_title(); ?></h2>-->
             <?php the_content(); ?>
-        </article>
+
         <?php
                 endwhile;
             endif;
